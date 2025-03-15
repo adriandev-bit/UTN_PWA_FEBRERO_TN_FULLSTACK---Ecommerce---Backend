1db2b7b129ae73576c4d9f96d81892c269fe9a66
Adrian.dev <adrianmogus@hotmail.com>
2025-03-14 21:55:10 -0300

    Corrección en rutas de vercel.json

--- FRONTEND/src/config/enviroment.js
+++
@@ -1,5 +0,0 @@
-const ENVIROMENT = {
-    URL_API: import.meta.env.VITE_URL_API
-}
-
-export default ENVIROMENT
--- FRONTEND/src/utils/error.util.js
+++
@@ -1,7 +0,0 @@
-export class ServerError extends Error {
-
-    constructor(message, status) {
-        super(message)
-        this.status = status
-    }
-}
